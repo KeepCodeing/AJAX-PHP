26a6725e0b67c76199d1c159e0d9349fa6f01e81
keepcodeing <3350680655@qq.com>
2019-06-11 14:37:56 +0800

    post/get测试

---
+++ First.php
@@ -0,0 +1,17 @@
+<?php
+// 数组，字典语法
+//$var1 = array(1, 2, 3);
+//print_r($var1);
+//$dic1 = array('name'=>'YJSP', 'age'=>'24');
+//echo '</br>'
+//print_r($dic1)
+
+//获取get请求（input控件需要有name属性（键），否则获取数据为空）
+//$_GET;
+//print_r($_GET);
+
+//获取post请求
+//$_POST;
+//print_r($_POST);
+
+?>
